f5a196db04b4007d7da5d2c71f7f8dc02efc4ac5
Some1Uknow <raghu250407@example.com>
2024-03-07 22:30:48 +0530

    used session data to display username, email and user image from google Oauth

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,19 +1,47 @@
+'use client'
 import Image from "next/image";
-import React from "react";
+import { IoCalendarNumberSharp } from "react-icons/io5";
+import { FaEarthAfrica } from "react-icons/fa6";
+import { IoIosMale, IoIosFemale } from "react-icons/io";
+import { useSession } from "next-auth/react";
 
 const ProfileCard = () => {
+
+  const { data: session} = useSession();
+
   return (
-    <section className=" shadow-md rounded-lg m-4 mr-1 p-3 h-56 w-80 max-w-full">
-      <div className="flex flex-row p-1">
-        <Image className="mr-3 rounded-full inline-block" height={40} width={44} alt="user-image" />
+    <section className=" shadow-md rounded-lg m-4 mr-1 p-3 h-max w-80 max-w-full">
+      <div className="flex flex-row p-1"> 
+        <Image
+          className="mr-3 rounded-full inline-block"
+          height={40}
+          width={44}
+          alt="user-image"
+          src={session?.user?.image}
+        />
         <div className="flex flex-col">
-          <p className=" text-xl font-bold">Raghav Sharma</p>
-          <p className=" text-xs text-gray-500">raghavsharma@email</p>
+          <p className="text-xl font-bold">{session.user.name}</p>
+          <p className="text-xs text-gray-500">{session.user.email}</p>
         </div>
       </div>
       <div className="p-1">
-        <p className="text-xs text-gray-600 p-1">This is user's bio, they can put all kind of stuff abt themselves</p>
-        <p className="p-1 text-gray-600">Male | 19</p>
+        <p className="text-xs text-gray-600 p-1">
+          This is user's bio, they can put all kind of stuff abt themselves
+        </p>
+        <div className="flex flex-row justify-between">
+          <p className=" text-gray-600 ">
+            <IoIosMale className="m-1 inline" />
+            Male
+          </p>
+          <p className=" text-gray-600 flex items-center">
+            <IoCalendarNumberSharp className="mr-1 inline" />
+            19
+          </p>
+          <p className=" text-gray-600 items-center flex">
+            <FaEarthAfrica className="mr-1 inline" />
+            India
+          </p>
+        </div>
       </div>
     </section>
   );
