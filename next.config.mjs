03a0ba210cf37df98f75f5b12b9f8f097a137c62
Some1Uknow <raghu250407@example.com>
2024-03-08 23:13:31 +0530

    added some mongodb config to avoid errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ const nextConfig = {
           },
         ],
       },
+      experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
+      webpack(config) {
+          config.experiments = { ...config.experiments, topLevelAwait: true };
+          return config;
+      }
 };
 
 export default nextConfig;
