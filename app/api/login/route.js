5cdad93ce454ba0747437cf0c42c4432d6c42005
Some1Uknow <raghu250407@gmail.com>
2024-03-29 00:27:55 +0530

    created login route

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,10 +1,32 @@
-import mongoose from "mongoose";
-import psychologist from "@/models/psychologist";
 import connectDB from "@/lib/mongodb";
+import Psychologist from "@/models/psychologist";
+import { NextResponse } from "next/server";
 
-export async function GET(request){
-    const {email, password} = await request.json();
+export async function POST(request) {
+  const { email, password } = await request.json();
+    console.log(request);
+  try {
     await connectDB();
-    await psychologist.findOne({email: email})
-    
+    const psychologist = await Psychologist.findOne({ email });
+
+    if (!psychologist) {
+      return NextResponse.json(
+        { message: "Psychologist not found bruh" },
+        { status: 404 }
+      );
+    }
+
+    if (psychologist.password !== password) {
+      return NextResponse.json({ message: "Wrong password" });
+    }
+// send psychologist ID once login is successfull
+    return NextResponse.json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Server error" });
+  }
+}
+
+export function methodNotAllowed(res) {
+  return NextResponse.methodNotAllowed(res);
 }
