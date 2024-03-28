c3697f52e48d676106b75c960b841dd3fe99ba98
Some1Uknow <raghu250407@gmail.com>
2024-03-28 17:18:54 +0530

    created login banner for psychologists

---
+++ components/Psychologist/Banner.jsx
@@ -0,0 +1,23 @@
+const Banner = () => {
+    return (
+      <div className="h-full p-24 pl-0 border-r-2">
+        <p className="font-Madimi mt-10 text-left text-pink-800">
+          <span className="text-8xl">Psychologists</span>
+          <br />
+          <span className="text-8xl">on VentOut</span>
+          <br />
+          <span className="text-7xl">get a lot of perks!</span>
+        </p>
+        <ul className="mt-16 list-none list-inside font-Poppins font-semibold flex flex-col gap-2.5 text-xl text-pink-600">
+          <li>- Expanded reach to clients worldwide</li>
+          <li>- Flexible scheduling using client's details</li>
+          <li>- Find patients according to your expertise</li>
+          <li>- Your data stays safe here</li>
+          <li>- We have the best support staff</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Banner;
+  
