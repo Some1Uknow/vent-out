f7106fa6fee328cd00b7dccb580a4e2cf9316f2b
Raghav Sharma <raghu250407@gmail.com>
2024-03-30 01:00:03 +0530

    made registration POST route and GET route to get all psychologist info

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -3,8 +3,43 @@ import Psychologist from "@/models/psychologist";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { firstName, lastName, email, password, expertise, country, years, phone, company } = await request.json();
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    expertise,
+    country,
+    years,
+    phone,
+    company,
+  } = await request.json();
   await connectDB();
-  await Psychologist.create({ firstName, lastName, email, password, expertise, country, years, phone, company });
-  return NextResponse.json({ message: "Psychologist Created" }, { status: 201 });
+  await Psychologist.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    expertise,
+    country,
+    years,
+    phone,
+    company,
+  });
+  return NextResponse.json(
+    { message: "Psychologist Created" },
+    { status: 201 }
+  );
+}
+
+export async function GET(request) {
+  await connectDB();
+  const psychologist = await Psychologist.find({});
+  if (!psychologist || psychologist.length === 0) {
+    return NextResponse.json(
+      { message: "Psychologists not present" },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ psychologist }, { status: 201 });
 }
