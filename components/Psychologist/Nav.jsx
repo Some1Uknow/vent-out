f65be17caae8d78658500dedd4602ecdfe0727f8
Raghav Sharma <raghu250407@gmail.com>
2024-03-30 01:01:23 +0530

    created a nav for psychologist page

---
+++ components/Psychologist/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='flex flex-row shadow-md rounded-lg p-3 m-4 justify-around mb-1 bg-white h-max'><button className='flex flex-row shadow-md rounded-lg p-1 m-1 justify-around bg-white'>Users</button></div>
+  )
+}
+
+export default Nav
