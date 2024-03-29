5b54ba4329e95c155f35f97c2c1551275974bc12
Raghav Sharma <raghu250407@gmail.com>
2024-03-30 01:00:41 +0530

    made the component dynamic by linking it with backend

--- components/Help.jsx
+++ components/Help.jsx
@@ -1,58 +1,61 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Help = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/register");
+      const psychologistData = await res.json();
+      setData(psychologistData.psychologist);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2 shadow-md rounded-lg p-3 m-4 justify-around mb-1 bg-white">
-      <span className="text-5xl font-bold">Introducing Proffessional Help!</span>
-      <span className="text-xl">
-        If you feel like its getting too much its better to get a psychologist
-        who can help you
+      <span className="text-5xl font-bold">
+        Introducing Professional Help!
       </span>
-      <Doctors
-        name="James Anderson"
-        years="5"
-        expertise="Anxiety and Self Perception Related Problems"
-        phoneno="4989xxx929"
-      />
-      <Doctors
-        name="Tim Southee"
-        years="15"
-        expertise="Helping People solve their Relationship problems"
-        phoneno="9894xxxx92"
-      />
-       <Doctors
-        name="Joel Garner"
-        years="25"
-        expertise="Mental Health expert,  specializes in treating chronic Depression"
-        phoneno="489xxxxx92"
-      />
+      <span className="text-xl">
+        If you feel like it's getting too much, it's better to get a psychologist who can help you.
+      </span>
+      {data.map((psychologist) => (
+        <Doctors key={psychologist._id} {...psychologist} />
+      ))}
     </div>
   );
 };
 
 export default Help;
 
-const Doctors = ({ name, years, expertise, phoneno }) => {
+const Doctors = ({ firstName, lastName, email, years, expertise, phone, country, company }) => {
+  const handleEmailClick = (email) => {
+    window.location.href = `mailto:${email}`;
+  };
   return (
     <div>
       <section className=" shadow-md rounded-lg p-2 h-max w-9/10 m-1 mt-4 bg-white border-t-2">
         <div className="flex flex-row p-1">
           <Image
-            className="rounded-full mr-2 object-cover"
-            height={24}
-            width={44}
+            className="object-cover w-34 h-34 border-2 mr-2 shadow-sm"
+            // height={24}
+            // width={84}
             alt="user-image"
-            //   src={imgSrc}
           />
           <div className="flex flex-col">
-            <p className="text-xl font-bold">{name}</p>
-            <p className="text-xs text-gray-500">{years} years of experience</p>
+            <p className="text-3xl font-bold">{firstName} {lastName}</p>
+            <p className="text-xl text-gray-500 font-semibold mb-1">{years} years of experience</p>
+            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1 cursor-pointer" onClick={() => handleEmailClick(email)}>Email: {email}</p>
+            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1">Country: {country}</p>
+            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1">Company: {company}</p>
           </div>
         </div>
         <div className="p-1 mb-2 flex flex-row justify-between">
-          <p className="text-sm text-gray-600 p-1">Expertise: {expertise}</p>
-          <p className="text-sm text-gray-600 p-1">Contact: {phoneno}</p>
+          <p className="text-2xl font-bold text-gray-600 p-1">Expertise: {expertise}</p>
+          <p className="text-xl text-gray-600 p-1">Contact: {phone}</p>
         </div>
       </section>
     </div>
