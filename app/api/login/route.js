b9e71ea83e1d8a02f6f18a8e8ae21ee68b912df1
Raghav Sharma <raghu250407@gmail.com>
2024-03-30 00:59:12 +0530

    made login route

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -4,14 +4,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { email, password } = await request.json();
-    console.log(request);
+
   try {
     await connectDB();
     const psychologist = await Psychologist.findOne({ email });
 
     if (!psychologist) {
       return NextResponse.json(
-        { message: "Psychologist not found bruh" },
+        { message: "Psychologist not found" },
         { status: 404 }
       );
     }
@@ -19,14 +19,11 @@ export async function POST(request) {
     if (psychologist.password !== password) {
       return NextResponse.json({ message: "Wrong password" });
     }
-// send psychologist ID once login is successfull
-    return NextResponse.json({ message: "Login successful" });
+
+    // Send psychologist ID once login is successful
+    return NextResponse.json({ message: "Login successful", psychologistId: psychologist._id });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "Server error" });
   }
-}
-
-export function methodNotAllowed(res) {
-  return NextResponse.methodNotAllowed(res);
 }
