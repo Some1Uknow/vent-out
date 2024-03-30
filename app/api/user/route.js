d96a77062199ce7772a96d8b06e753f729aba943
Raghav Sharma <raghu250407@gmail.com>
2024-03-31 00:20:21 +0530

    created get route to get all the user data for psychologist

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,10 +1,21 @@
 import connectDB from "@/lib/mongodb";
 import User from "@/models/user";
+import profileSchema from "@/models/profile";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { name, email } = await request.json();
   await connectDB();
   await User.create({ name, email });
-  return NextResponse.json({ message: "User Created" }, { status: 201 });
+  return new NextResponse.json({ message: "User Created" }, { status: 201 });
+}
+
+export async function GET(request) {
+  await connectDB();
+  const Users = await profileSchema.find({});
+  const Users1 = await User.find({});
+  if (!Users || !Users.length) {
+    return NextResponse.json({ message: "Cant fetch users" }, { status: 201 });
+  }
+  return NextResponse.json({ Users, Users1 }, { status: 201 });
 }
