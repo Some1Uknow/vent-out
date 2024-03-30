7fd79d5831ab46b296970cb38a2f0d6081f526ec
Raghav Sharma <raghu250407@gmail.com>
2024-03-31 00:20:51 +0530

    added vector image

--- components/Help.jsx
+++ components/Help.jsx
@@ -40,17 +40,18 @@ const Doctors = ({ firstName, lastName, email, years, expertise, phone, country,
       <section className=" shadow-md rounded-lg p-2 h-max w-9/10 m-1 mt-4 bg-white border-t-2">
         <div className="flex flex-row p-1">
           <Image
-            className="object-cover w-34 h-34 border-2 mr-2 shadow-sm"
-            // height={24}
-            // width={84}
+            className="rounded-full border-2 mr-4 shadow-sm"
+            src="https://static.vecteezy.com/system/resources/previews/002/002/427/non_2x/man-avatar-character-isolated-icon-free-vector.jpg"
+            width={225}
+            height={100}
             alt="user-image"
           />
           <div className="flex flex-col">
-            <p className="text-3xl font-bold">{firstName} {lastName}</p>
-            <p className="text-xl text-gray-500 font-semibold mb-1">{years} years of experience</p>
-            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1 cursor-pointer" onClick={() => handleEmailClick(email)}>Email: {email}</p>
-            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1">Country: {country}</p>
-            <p className="text-sm border-2 rounded-lg p-1 text-gray-500 mb-1">Company: {company}</p>
+            <p className="text-4xl mb-1 font-bold">{firstName} {lastName}</p>
+            <p className="text-xl text-gray-500 font-semibold mb-2">{years} years of experience</p>
+            <p className="text-md border-2 rounded-lg p-2 font-semibold text-gray-50 bg-gray-700 mb-2 cursor-pointer" onClick={() => handleEmailClick(email)}>Email: {email}</p>
+            <p className="text-md border-2 rounded-lg p-2 font-semibold text-gray-50 bg-gray-700 mb-2">Country: {country}</p>
+            <p className="text-md border-2 rounded-lg p-2 font-semibold text-gray-50 bg-gray-700 mb-2">Company: {company}</p>
           </div>
         </div>
         <div className="p-1 mb-2 flex flex-row justify-between">
