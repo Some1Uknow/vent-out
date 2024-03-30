b8b76a340303038c882106098b131a56da6b7677
Raghav Sharma <raghu250407@gmail.com>
2024-03-31 00:21:55 +0530

    card completed

--- components/Psychologist/PsychologistCard.jsx
+++ components/Psychologist/PsychologistCard.jsx
@@ -1,29 +1,52 @@
 import { MdOutlinePeople } from "react-icons/md";
 import { IoCalendarNumberSharp } from "react-icons/io5";
 import { FaEarthAfrica } from "react-icons/fa6";
+import Image from "next/image";
 
 const PsychologistCard = ({ psychologist }) => {
-  const { firstName, lastName, email, expertise, years, country, phone, createdAt } = psychologist;
+  const {
+    firstName,
+    lastName,
+    email,
+    expertise,
+    years,
+    country,
+    phone,
+    createdAt,
+  } = psychologist;
+
 
   return (
-    <section className="shadow-md rounded-lg m-4 mr-1 p-3 h-max w-max bg-white">
+    <section className="shadow-md rounded-lg m-4 mr-1 p-3 h-max w-1/4 bg-white">
       <div className="flex flex-row p-2">
         <div className="mr-3 rounded-full inline-block bg-gray-200">
           {/* Use placeholder image or user's actual image */}
-          <img
-            className="h-20 w-20 rounded-full object-cover"
+          <Image
+            className="rounded-full object-cover"
             src="https://via.placeholder.com/60"
             alt="user-image"
+            width={120}
+            height={60}
           />
         </div>
         <div className="flex flex-col">
-          <p className="text-3xl font-bold font-Poppins">{firstName} {lastName}</p>
-          <p className="text-xl text-gray-500">{email}</p>
+          <p className="text-3xl font-bold font-Poppins">
+            {firstName} {lastName}
+          </p>
+          <p
+            className="text-xl text-gray-500 cursor-pointer"
+           
+          >
+            {email}
+          </p>
         </div>
       </div>
       <div className="p-1">
         <div className="flex flex-col justify-between">
-          <p className=" text-gray-600 flex items-center">Expertise:&nbsp;<MdOutlinePeople className="inline" />{expertise}
+          <p className=" text-gray-600 flex items-center">
+            Expertise:&nbsp;
+            <MdOutlinePeople className="inline" />
+            {expertise}
           </p>
           <p className=" text-gray-600 items-center flex">
             <IoCalendarNumberSharp className="mr-1 inline" />
