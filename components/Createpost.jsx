efed0ee998e45731a8d6fb511bdd459f04cd172e
Raghav Sharma <raghu250407@gmail.com>
2024-04-03 14:55:56 +0530

    css tweaks

--- components/Createpost.jsx
+++ components/Createpost.jsx
@@ -1,22 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 const CreatePost = () => {
   const { data: session } = useSession();
 
   const router = useRouter();
 
-  const createpost = () => {
+  const create = () => {
     router.push(`/create-post/${session?.user.name}`);
   };
   return (
     <>
       <button
-        onClick={() => createpost()}
-        className="transition hover:duration-300 hover:bg-gray-300 text-black bg-transparent font-bold py-2 rounded"
+        onClick={() => create()}
+        className="transition border-r-2 p-3 hover:duration-300 hover:bg-gray-300 text-black bg-transparent mb-1 font-bold rounded"
       >
-        <span className="font-Madimi text-xl m-2 p-4">Create Post</span>
+        <span className="font-Poppins text-2xl flex flex-row justify-center items-center">
+          <AiFillPlusCircle className="mr-2" />
+          Create Post
+        </span>
       </button>
     </>
   );
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,60 +1,22 @@
-'use client'
+"use client";
 import { useState } from "react";
 import ProfileCard from "./ProfileCard";
 import PostPage from "./PostPage";
 import News from "./News";
-import Communities from "./Communities";
-import Help from "./Help";
-import Products from "./Products";
+
+import Navbar from "./Navbar";
 
 const Main = () => {
   const [component, setComponent] = useState(<PostPage />);
 
   return (
     <div className="flex flex-row h-max font-Poppins justify-center bg-gray-100">
-      <ProfileCard />
-      <div className="flex flex-col w-3/5">
+      <div className="flex flex-col">
+        <ProfileCard />
         <Navbar setComponent={setComponent} />
-        {component}
       </div>
+      <div className="flex flex-col w-3/5">{component}</div>
       <News />
-    </div>
-  );
-};
-
-const Navbar = ({ setComponent }) => {
-  return (
-    <div className="flex flex-row shadow-md rounded-lg p-3 m-4 justify-around mb-1 bg-white">
-      <button
-        onClick={() => setComponent(<PostPage />)}
-        className="transition hover:duration-300 hover:bg-gray-300 text-black bg-transparent font-bold py-2 rounded"
-      >
-        {" "}
-        <span className="font-Poppins text-xl m-2 p-4">Posts</span>
-      </button>
-      <button
-        onClick={() => setComponent(<Communities />)}
-        className="transition hover:duration-300 hover:bg-gray-300 text-black bg-transparent font-bold py-2 rounded"
-      >
-        {" "}
-        <span className="font-Poppins text-xl m-2 p-4">Communities</span>
-      </button>
-      <button
-        onClick={() => setComponent(<Help />)}
-        className="transition hover:duration-300 hover:bg-gray-300 text-black bg-transparent font-bold py-2 rounded"
-      >
-        {" "}
-        <span className="font-Poppins text-xl m-2 p-4">
-          Get Professional Help
-        </span>
-      </button>
-      <button
-        onClick={() => setComponent(<Products />)}
-        className="transition hover:duration-300 hover:bg-gray-300 text-black bg-transparent font-bold py-2 rounded"
-      >
-        {" "}
-        <span className="font-Poppins text-xl m-2 p-4">Products</span>
-      </button>
     </div>
   );
 };
