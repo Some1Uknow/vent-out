07fe0f321cebb70fc5003df2965d22224c48d6c4
Raghav Sharma <raghu250407@gmail.com>
2024-04-03 14:56:45 +0530

    fixed import statements

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { IoCalendarNumberSharp } from "react-icons/io5";
 import { FaEarthAfrica } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
-import CreatePost from "./CreatePost";
+import Createpost from "./Createpost";
 import Profilebtn from "./Profilebtn";
 import { MdOutlinePeople } from "react-icons/md";
 import { useEffect, useState } from "react";
@@ -34,47 +34,51 @@ const ProfileCard = ({ params }) => {
     fetchData();
   }, []);
 
-
   return (
     <div className="flex flex-col items-center">
-      <section className=" shadow-md rounded-lg m-4 mr-1 p-3 h-max w-80 max-w-full bg-white">
-        <div className="flex flex-row p-2">
+      <section className="shadow-md rounded-lg mt-4 mb-4 w-9/10 bg-white">
+        <div className="flex flex-col items-center relative">
+          <div className="overflow-hidden w-full h-2/3 absolute top-0 left-0">
+            <div className="bg-pink-600 w-full h-20"></div>
+          </div>
           <Image
-            className="mr-3 rounded-full inline-block"
-            height={40}
-            width={44}
+            className="rounded-full mt-2 z-10"
+            height={160}
+            width={160}
             alt="user-image"
             src={session?.user?.image}
           />
-          <div className="flex flex-col">
-            <p className="text-xl font-bold">{session?.user.name}</p>
-            <p className="text-xs text-gray-500">{session?.user.email}</p>
+          <div className="flex flex-col items-center">
+            <p className="text-4xl font-bold mt-2">{session?.user.name}</p>
+            <p className="text-xl text-gray-500 p-1">{session?.user.email}</p>
           </div>
         </div>
-        <div className="p-1">
-          <p className="text-sm text-gray-600 p-1 mb-2">{profileData.bio}</p>
-          <div className="flex flex-row justify-between">
-            <p className=" text-gray-600 ">
-              <MdOutlinePeople  className="m-1 inline" />
+        <div className="p-1 flex flex-col justify-around">
+          <div className="flex flex-col items-center p-1">
+            <p className="text-xl text-gray-600 p-1 mb-2 text-center font-semibold">{profileData.bio}</p>
+          </div>
+          <div className="flex flex-row items-center text-xl mb-2 justify-around">
+            <p className="text-gray-600 flex items-center bg-pink-200 p-2 rounded-full">
+              <MdOutlinePeople className="mr-1 inline" />
               {profileData.gender}
             </p>
-            <p className=" text-gray-600 flex items-center">
+            <p className="text-gray-600 flex items-center bg-pink-200 p-2 rounded-full">
               <IoCalendarNumberSharp className="mr-1 inline" />
-              {profileData.age}
+              Age: {profileData.age} 
             </p>
-            <p className=" text-gray-600 items-center flex">
+            <p className="text-gray-600 flex items-center bg-pink-200 p-2 rounded-full">
               <FaEarthAfrica className="mr-1 inline" />
               {profileData?.country}
             </p>
           </div>
         </div>
-      </section>
-      <div className=" border-t-2 flex flex-row shadow-md rounded-lg mt-0 m-4 mr-1 p-3 h-max w-80 max-w-full bg-white">
-        <CreatePost />
+        <div className="flex flex-row shadow-md justify-around p-2 mt-3 border-t-2 rounded-lg w-full bg-white">
+        <Createpost />
         <Profilebtn />
       </div>
+      </section>
+      
     </div>
   );
-};
-
+  };  
 export default ProfileCard;
