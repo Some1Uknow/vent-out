acfde9b3876f6a90bc97b780c6decddb629958d4
Some1Uknow <raghu250407@gmail.com>
2024-06-03 17:30:15 +0530

    google verified

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,6 +4,8 @@ import connecting from "@/public/connecting.webp";
 import emotions from "@/public/emotions.webp";
 import mentalHealth from "@/public/mental-health.webp";
 import community from "@/public/community.jpg";
+import { CgSmile } from "react-icons/cg";
+import { FaSmile } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -13,10 +15,10 @@ const Home = () => {
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
           <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
           <h1 className="font-Poppins text-7xl text-center bg-pink-600 border-b-2 shadow-md p-36 bg-clip-text text-transparent relative z-10">
-            Feeling down? We are here to help <BiSmile/>
+            Feeling down? We are here to help 
           </h1>
         </div>
-
+        <div><FaSmile/></div>
         <div className="self-start text-center flex flex-col justify-center gap-12 mt-10 w-full p-12">
           <Card
             icon={<BiHappyBeaming />}
